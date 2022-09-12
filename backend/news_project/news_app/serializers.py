0fdea02230cd849c2185d5167c5e391d9e968e3e
Anton Hrushchev <a.hrushchev@dunice.net>
2022-09-12 12:55:30 +0300

    improved serializer: create post and create tags at once

--- backend/news_project/news_app/serializers.py
+++ backend/news_project/news_app/serializers.py
@@ -5,7 +5,7 @@ from news_app.models import Post, Tag, User
 class TagSerializer(serializers.ModelSerializer):
     class Meta:
         model        = Tag
-        fields       = '__all__'
+        fields       = ('id', 'title')
 
 class PostSerializer(serializers.ModelSerializer):
     tags = TagSerializer(many=True)
@@ -13,11 +13,27 @@ class PostSerializer(serializers.ModelSerializer):
         model        = Post
         fields       = '__all__'
 
+    def create(self, validated_data):
+        tags_data = validated_data.pop('tags')
+        post = Post.objects.create(**validated_data)
+        tags_to_apply = []
+
+        for tag_data in tags_data:
+            Tag.objects.get_or_create(**tag_data)
+            current_tag = Tag.objects.filter(title=tag_data['title']).values_list('id', flat=True)
+            print(current_tag)
+            tags_to_apply += current_tag
+
+        for tag in tags_to_apply:
+            post.tags.add(tag)
+
+        return post
+
 class UserProfileSerializer(serializers.ModelSerializer):    
     posts = PostSerializer(many=True, required=False)
     class Meta:
         model        = User
-        fields       = ('username', 'email', 'profile_image', 'posts')
+        fields       = ('id', 'username', 'email', 'profile_image', 'posts')
         extra_kwargs = {
             'password':{
                 'write_only':'True',
